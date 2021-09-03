178aa964a122b1f8565097a9df50cdc934ae64ef
Vladimir Alimpic <vladimir96ks@gmail.com>
2021-09-03 16:52:57 +0200

    minor update

--- src/main.js
+++ src/main.js
@@ -465,7 +465,7 @@ const messages = {
           info: 'Paper tray for use only with DNP DS620'
         },
         {
-          name: ['DS620 Roll Holder1', '(Long/Left)'],
+          name: ['DS620 Roll Holder 1', '(Long/Left)'],
           path: 'ds620-roll-holder-1-(long/left)',
           image: '91-thickbox_default.jpg',
           reference: '23208760S',
@@ -473,7 +473,7 @@ const messages = {
           info: 'DS620  Roll Holder 1 (Long/Left)'
         },
         {
-          name: ['DS620 Roll Holder2', 'Short/Right'],
+          name: ['DS620 Roll Holder 2', 'Short/Right'],
           path: 'ds620-roll-holder-2-short/right',
           image: '92-thickbox_default.jpg',
           reference: '23208770S',
